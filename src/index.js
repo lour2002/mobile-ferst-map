72634ed89f5b94427244cbd5c2dd8976d55b3773
Boris Avsiukevych <boris.avs@live.ru>
2019-06-30 23:13:42 +0300

    Create component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
+/* Styles */
 import './styles.scss';
+
+import {Map} from './modules/ClassMap';
+
+window.onload = () => {
+    const map = new Map('mapbox');
+
+    document.getElementById('js-add-point').addEventListener("click", () => map.addPoint() );
+    document.getElementById('js-add-problem').addEventListener("click", () => {
+        const LngLat = map.newPointLngLat;
+
+        if (LngLat.length) {
+            let points = localStorage.getItem('__mapPoints');
+            console.log(points)
+            if (null === points) {
+                points = [LngLat];
+                localStorage.setItem('__mapPoints', JSON.stringify(points))
+            } else if ('string' === typeof points && points.length){
+                try {
+                    points = JSON.parse(points);
+                    points.push(LngLat)
+                    localStorage.setItem('__mapPoints', JSON.stringify(points))
+                } catch (error) {
+                    console.error(`Can't get point from localStorage`);
+                }
+                
+            }
+        }
+
+        map.resetNewPoint();
+    });
+}
